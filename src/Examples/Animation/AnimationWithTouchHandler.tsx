27114e55dee36e141d08452f275a2a5b38bde158
Edwards Moses <edwardsmoses3@gmail.com>
2022-05-18 20:55:12 +0100

    finally progress

--- src/Examples/Animation/AnimationWithTouchHandler.tsx
+++ src/Examples/Animation/AnimationWithTouchHandler.tsx
@@ -1,32 +1,39 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { Canvas, Fill, useTouchHandler, Path, Skia, PathVerb, useCanvasRef } from "@shopify/react-native-skia";
+import {
+  Canvas,
+  Fill,
+  useTouchHandler,
+  Path,
+  Skia,
+  PathVerb,
+  useCanvasRef,
+  useValue,
+} from "@shopify/react-native-skia";
 
 import { AnimationDemo } from "./Components";
 
 export const AnimationWithTouchHandler = () => {
-  const path = Skia.Path.Make();
-
   const canvasRef = useCanvasRef();
+
+  const drawPath = useValue("");
 
   // Touch handler
   const touchHandler = useTouchHandler({
     onStart: ({ x, y }) => {
-      path.moveTo(x, y);
+      drawPath.current = `${drawPath.current} M ${x} ${y}`;
     },
     onActive: ({ x, y }) => {
-      path.lineTo(x, y);
+      drawPath.current = `${drawPath.current} L ${x} ${y}`;
     },
-    onEnd: ({}) => {
-      path.close();
-    },
+    onEnd: ({}) => {},
   });
 
   return (
     <AnimationDemo title={"Bouncing animation with touch handler"}>
       <Canvas style={styles.canvas} ref={canvasRef} onTouch={touchHandler}>
         <Fill color="white" />
-        <Path path={path} color="lightblue" />
+        <Path path={drawPath} color="lightblue" style={"stroke"} />
       </Canvas>
     </AnimationDemo>
   );
